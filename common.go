fd94ccf1aa3ff1269aaa5eb528e908d3931e6c7f
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-03-26 23:00:00 +0300

    Add the `killBackgroundProcess()` function

--- common.go
+++ common.go
@@ -24,3 +24,24 @@ func findBackgroundProcess() (ps.Process, error) {
 
 	return backgroundProcess, nil
 }
+
+func killBackgroundProcess() error {
+	backgroundProcess, err := findBackgroundProcess()
+	if err != nil {
+		return fmt.Errorf("unable to find a background process: %w", err)
+	}
+	if backgroundProcess == nil {
+		return nil
+	}
+
+	osBackgroundProcess, err := os.FindProcess(backgroundProcess.Pid())
+	if err != nil {
+		return fmt.Errorf("unable to find a background process by its PID: %w", err)
+	}
+
+	if err := osBackgroundProcess.Kill(); err != nil {
+		return fmt.Errorf("unable to kill a background process: %w", err)
+	}
+
+	return nil
+}
--- stop_command.go
+++ stop_command.go
@@ -1,29 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
 type stopCommand struct{}
 
 func (command stopCommand) Run() error {
-	backgroundProcess, err := findBackgroundProcess()
-	if err != nil {
-		return fmt.Errorf("unable to find a background process: %w", err)
-	}
-	if backgroundProcess == nil {
-		return nil
-	}
-
-	osBackgroundProcess, err := os.FindProcess(backgroundProcess.Pid())
-	if err != nil {
-		return fmt.Errorf("unable to find a background process by its PID: %w", err)
-	}
-
-	if err := osBackgroundProcess.Kill(); err != nil {
-		return fmt.Errorf("unable to kill a background process: %w", err)
-	}
-
-	return nil
+	return killBackgroundProcess()
 }
