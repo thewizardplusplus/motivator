508f02ffd6d836e436cd302f07873e3336d4a2dd
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-03-26 23:00:00 +0300

    Fix the order of the commands in the help message

--- main.go
+++ main.go
@@ -10,10 +10,10 @@ import (
 )
 
 type cli struct {
-	Foreground foregroundCommand `kong:"cmd,help='Start showing notifications in foreground.'"`              // nolint: lll
 	Start      startCommand      `kong:"cmd,help='Start (or restart) showing notifications in background.'"` // nolint: lll
 	Status     statusCommand     `kong:"cmd,help='Check that notifications are being show in background.'"`  // nolint: lll
 	Stop       stopCommand       `kong:"cmd,help='Stop showing notifications in background.'"`               // nolint: lll
+	Foreground foregroundCommand `kong:"cmd,help='Start showing notifications in foreground.'"`              // nolint: lll
 }
 
 func main() {
