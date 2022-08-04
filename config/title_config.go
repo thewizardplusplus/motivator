01fe05035bb952155228522dd39a2d1b9c3160b2
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-08-04 04:00:39 +0300

    Add the `config.TitleConfig.Title()` method

--- config/title_config.go
+++ config/title_config.go
@@ -1,6 +1,29 @@
 package config
 
+import (
+	"strings"
+
+	"github.com/thewizardplusplus/motivator/entities"
+)
+
 type TitleConfig struct {
 	HideAppName         bool
 	UseOriginalTaskName bool
+}
+
+func (config TitleConfig) Title(appName string, task entities.Task) string {
+	taskName := task.SelectedName()
+	if config.UseOriginalTaskName {
+		taskName = task.OriginalName
+	}
+
+	var titleParts []string
+	if !config.HideAppName {
+		titleParts = append(titleParts, appName)
+	}
+	if taskName != "" {
+		titleParts = append(titleParts, taskName)
+	}
+
+	return strings.Join(titleParts, " | ")
 }
