75a3f9174521ad47a024bcd0a255b07c3456f913
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-08-04 03:53:34 +0300

    Add the `entities.Task.makeJobPlanner()` method

--- entities/task.go
+++ entities/task.go
@@ -8,6 +8,15 @@ import (
 	"github.com/go-co-op/gocron"
 )
 
+const cronWithSecondsFieldCount = 6
+
+type jobPlannerType func(jobHandler interface{}, parameters ...interface{}) (
+	*gocron.Job,
+	error,
+)
+
+type cronParserType func(cronExpression string) *gocron.Scheduler
+
 type Task struct {
 	Name            string
 	OriginalName    string `json:"-"`
@@ -35,31 +44,26 @@ func (task Task) PlanJob(
 	scheduler *gocron.Scheduler,
 	taskHandler func(task Task),
 ) (*gocron.Job, error) {
-	type jobPlannerType func(jobHandler interface{}, parameters ...interface{}) (
-		*gocron.Job,
-		error,
-	)
-	type cronParserType func(cronExpression string) *gocron.Scheduler
-
-	var jobPlanner jobPlannerType
-	if task.Cron != "" {
-		var cronParser cronParserType
-		const cronWithSecondsFieldCount = 6
-		if fields := strings.Fields(task.Cron); len(fields) == cronWithSecondsFieldCount { // nolint: lll
-			cronParser = scheduler.CronWithSeconds
-		} else {
-			cronParser = scheduler.Cron
-		}
-
-		jobPlanner = cronParser(task.Cron).Do
-	} else {
-		jobPlanner = scheduler.Every(task.Delay).Do
-	}
-
+	jobPlanner := task.makeJobPlanner(scheduler)
 	job, err := jobPlanner(func() { taskHandler(task) })
 	if err != nil {
 		return nil, fmt.Errorf("unable to start the job scheduler: %w", err)
 	}
 
 	return job, nil
+}
+
+func (task Task) makeJobPlanner(scheduler *gocron.Scheduler) jobPlannerType {
+	if task.Cron == "" { // the `Cron` field has priority over the `Delay` field
+		return scheduler.Every(task.Delay).Do
+	}
+
+	var cronParser cronParserType
+	if fields := strings.Fields(task.Cron); len(fields) == cronWithSecondsFieldCount { // nolint: lll
+		cronParser = scheduler.CronWithSeconds
+	} else {
+		cronParser = scheduler.Cron
+	}
+
+	return cronParser(task.Cron).Do
 }
